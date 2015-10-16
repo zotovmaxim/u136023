8878dc59cb62ed22b04937abb4861d40386c816f
Zotov Maxim <shynox@inbox.ru>
2015-10-16 18:24:17 +0300

    git-repository creation

---
+++ .access.php
@@ -0,0 +1,10 @@
+<?
+$PERM["upload"]["*"]="R";
+$PERM["/"]["12"]="T_10";
+$PERM["/"]["11"]="R";
+$PERM["/"]["*"]="D";
+$PERM["/"]["2"]="D";
+$PERM["about"]["8"]="T_9";
+$PERM["company"]["8"]="T_9";
+$PERM["desktop_app"]["*"]="R";
+?>
---
+++ bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001445004845';
+$dateexpire = '001445015645';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:4:{s:22:"C_R_2015-10-17_BYR_RUB";a:4:{s:11:"curFromRate";d:39.439999999999998;s:14:"curFromRateCnt";i:10000;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-10-17_EUR_RUB";a:4:{s:11:"curFromRate";d:62.770000000000003;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-10-17_UAH_RUB";a:4:{s:11:"curFromRate";d:24.530000000000001;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-10-17_USD_RUB";a:4:{s:11:"curFromRate";d:57.390000000000001;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
+return true;
+?>
