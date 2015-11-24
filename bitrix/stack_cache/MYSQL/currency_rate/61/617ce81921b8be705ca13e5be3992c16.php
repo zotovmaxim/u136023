ee3b39da6b4a3a3a0eff89672908dcc30006b5ab
bitrix <bitrix@localhost.localdomain>
2015-11-24 13:37:37 +0300

    if, hb was created

--- bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
+++ bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001447934930';
-$dateexpire = '001447945730';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:4:{s:22:"C_R_2015-11-20_BYR_RUB";a:4:{s:11:"curFromRate";d:39.439999999999998;s:14:"curFromRateCnt";i:10000;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-20_EUR_RUB";a:4:{s:11:"curFromRate";d:62.770000000000003;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-20_UAH_RUB";a:4:{s:11:"curFromRate";d:24.530000000000001;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-20_USD_RUB";a:4:{s:11:"curFromRate";d:57.390000000000001;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
+$datecreate = '001448360336';
+$dateexpire = '001448371136';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:4:{s:22:"C_R_2015-11-25_BYR_RUB";a:4:{s:11:"curFromRate";d:39.439999999999998;s:14:"curFromRateCnt";i:10000;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-25_EUR_RUB";a:4:{s:11:"curFromRate";d:62.770000000000003;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-25_UAH_RUB";a:4:{s:11:"curFromRate";d:24.530000000000001;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-11-25_USD_RUB";a:4:{s:11:"curFromRate";d:57.390000000000001;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
 return true;
 ?>
